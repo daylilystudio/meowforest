7ebace1fa5c6a9cbce306de095070fb3f3b5f50d
Daylily <daylily@mac>
2022-11-05 14:25:07 +0800

    Updates

---
+++ assets/LoginAdmin.89736a71.js
@@ -0,0 +1,41 @@
+import{f as S,g as p,h as T,i as f,d as v,u as N,j as A,k as B,l as y,m as L,t as E,n as l,q as K,s as k,v as V,w as g,x as W,y as M,z as P,A as H,N as D,T as G,B as O,C as F,_ as U,r as d,o as q,D as J,E as u,G as X,H as _,b as c,I as z,a as Y,J as Q,K as Z,L as R}from"./index.b4fadd1f.js";import{N as ee,a as te,b as ne,c as se}from"./FormItem.bc777681.js";import{f as h,u as oe}from"./get.3ae0ca1f.js";const ie=Array.apply(null,{length:24}).map((e,t)=>{const s=t+1,o=`calc(100% / 24 * ${s})`;return[f(`${s}-span`,{width:o}),f(`${s}-offset`,{marginLeft:o}),f(`${s}-push`,{left:o}),f(`${s}-pull`,{right:o})]}),re=S([p("row",{width:"100%",display:"flex",flexWrap:"wrap"}),p("col",{verticalAlign:"top",boxSizing:"border-box",display:"inline-block",position:"relative",zIndex:"auto"},[T("box",{position:"relative",zIndex:"auto",width:"100%",height:"100%"}),ie])]),I=K("n-row"),ae={gutter:{type:[Array,Number,String],default:0},alignItems:String,justifyContent:String},le=v({name:"Row",props:ae,setup(e){const{mergedClsPrefixRef:t,mergedRtlRef:s}=N(e);A("-legacy-grid",re,t);const o=B("Row",s,t),r=y(()=>{const{gutter:n}=e;return Array.isArray(n)&&n[1]||0}),i=y(()=>{const{gutter:n}=e;return Array.isArray(n)?n[0]:Number(n)});return L(I,{mergedClsPrefixRef:t,gutterRef:E(e,"gutter"),verticalGutterRef:r,horizontalGutterRef:i}),{mergedClsPrefix:t,rtlEnabled:o,styleMargin:y(()=>`-${h(r.value,{c:.5})} -${h(i.value,{c:.5})}`),styleWidth:y(()=>`calc(100% + ${h(i.value)})`)}},render(){return l("div",{class:[`${this.mergedClsPrefix}-row`,this.rtlEnabled&&`${this.mergedClsPrefix}-row--rtl`],style:{margin:this.styleMargin,width:this.styleWidth,alignItems:this.alignItems,justifyContent:this.justifyContent}},this.$slots)}}),ce={span:{type:[String,Number],default:1},push:{type:[String,Number],default:0},pull:{type:[String,Number],default:0},offset:{type:[String,Number],default:0}},de=v({name:"Col",props:ce,setup(e){const t=k(I,null);return t||V("col","`n-col` must be placed inside `n-row`."),{mergedClsPrefix:t.mergedClsPrefixRef,gutter:t.gutterRef,stylePadding:g(()=>`${h(t.verticalGutterRef.value,{c:.5})} ${h(t.horizontalGutterRef.value,{c:.5})}`),mergedPush:g(()=>Number(e.push)-Number(e.pull))}},render(){const{$slots:e,span:t,mergedPush:s,offset:o,stylePadding:r,gutter:i,mergedClsPrefix:n}=this;return l("div",{class:[`${n}-col`,{[`${n}-col--${t}-span`]:!0,[`${n}-col--${s}-push`]:s>0,[`${n}-col--${-s}-pull`]:s<0,[`${n}-col--${o}-offset`]:o}],style:{padding:r}},i?l("div",null,e):e)}}),ue=e=>{const{opacityDisabled:t,heightTiny:s,heightSmall:o,heightMedium:r,heightLarge:i,heightHuge:n,primaryColor:a,fontSize:m}=e;return{fontSize:m,textColor:a,sizeTiny:s,sizeSmall:o,sizeMedium:r,sizeLarge:i,sizeHuge:n,color:a,opacitySpinning:t}},pe={name:"Spin",common:W,self:ue},me=pe,fe=S([S("@keyframes spin-rotate",`
+ from {
+ transform: rotate(0);
+ }
+ to {
+ transform: rotate(360deg);
+ }
+ `),p("spin-container",{position:"relative"},[p("spin-body",`
+ position: absolute;
+ top: 50%;
+ left: 50%;
+ transform: translateX(-50%) translateY(-50%);
+ `,[M()])]),p("spin-body",`
+ display: inline-flex;
+ align-items: center;
+ justify-content: center;
+ flex-direction: column;
+ `),p("spin",`
+ display: inline-flex;
+ height: var(--n-size);
+ width: var(--n-size);
+ font-size: var(--n-size);
+ color: var(--n-color);
+ `,[f("rotate",`
+ animation: spin-rotate 2s linear infinite;
+ `)]),p("spin-description",`
+ display: inline-block;
+ font-size: var(--n-font-size);
+ color: var(--n-text-color);
+ transition: color .3s var(--n-bezier);
+ margin-top: 8px;
+ `),p("spin-content",`
+ opacity: 1;
+ transition: opacity .3s var(--n-bezier);
+ pointer-events: all;
+ `,[f("spinning",`
+ user-select: none;
+ -webkit-user-select: none;
+ pointer-events: none;
+ opacity: var(--n-opacity-spinning);
+ `)])]),ge={small:20,medium:18,large:16},he=Object.assign(Object.assign({},P.props),{description:String,stroke:String,size:{type:[String,Number],default:"medium"},show:{type:Boolean,default:!0},strokeWidth:Number,rotate:{type:Boolean,default:!0},spinning:{type:Boolean,validator:()=>!0,default:void 0}}),ye=v({name:"Spin",props:he,setup(e){const{mergedClsPrefixRef:t,inlineThemeDisabled:s}=N(e),o=P("Spin","-spin",fe,me,e,t),r=g(()=>{const{size:n}=e,{common:{cubicBezierEaseInOut:a},self:m}=o.value,{opacitySpinning:b,color:w,textColor:$}=m,C=typeof n=="number"?O(n):m[F("size",n)];return{"--n-bezier":a,"--n-opacity-spinning":b,"--n-size":C,"--n-color":w,"--n-text-color":$}}),i=s?H("spin",g(()=>{const{size:n}=e;return typeof n=="number"?String(n):n[0]}),r,e):void 0;return{mergedClsPrefix:t,compitableShow:oe(e,["spinning","show"]),mergedStrokeWidth:g(()=>{const{strokeWidth:n}=e;if(n!==void 0)return n;const{size:a}=e;return ge[typeof a=="number"?"medium":a]}),cssVars:s?void 0:r,themeClass:i==null?void 0:i.themeClass,onRender:i==null?void 0:i.onRender}},render(){var e,t;const{$slots:s,mergedClsPrefix:o,description:r}=this,i=s.icon&&this.rotate,n=(r||s.description)&&l("div",{class:`${o}-spin-description`},r||((e=s.description)===null||e===void 0?void 0:e.call(s))),a=s.icon?l("div",{class:[`${o}-spin-body`,this.themeClass]},l("div",{class:[`${o}-spin`,i&&`${o}-spin--rotate`],style:s.default?"":this.cssVars},s.icon()),n):l("div",{class:[`${o}-spin-body`,this.themeClass]},l(D,{clsPrefix:o,style:s.default?"":this.cssVars,stroke:this.stroke,"stroke-width":this.mergedStrokeWidth,class:`${o}-spin`}),n);return(t=this.onRender)===null||t===void 0||t.call(this),s.default?l("div",{class:[`${o}-spin-container`,this.themeClass],style:this.cssVars},l("div",{class:[`${o}-spin-content`,this.compitableShow&&`${o}-spin-content--spinning`]},s),l(G,{name:"fade-in-transition"},{default:()=>this.compitableShow?a:null})):a}}),ve=v({components:{NSpace:ee,NForm:te,NFormItem:ne,NInput:se,NRow:le,NCol:de,NButton:X,NSpin:ye},setup(){const e=k("axios");let t=_(!1);const s=Z(),o=_({username:"",password:""});return{loading:t,model:o,login(){t.value=!0;const r="https://vue3-course-api.hexschool.io/admin/signin";e.post(r,o.value).then(i=>{if(t.value=!1,!i.data.success)window.$notification.error({content:i.data.message,meta:i.data.error.message,duration:2500,keepAliveOnHover:!0});else{window.$notification.success({content:i.data.message,duration:2e3});const{token:n,expired:a}=i.data;document.cookie=`meowforestToken=${n}; expires=${new Date(a)}`,s.push("/admin")}}).catch(i=>{t.value=!1,console.log(i)})}}}}),be={class:"tw-flex tw-justify-end"};function we(e,t,s,o,r,i){const n=d("n-input"),a=d("n-form-item"),m=d("n-button"),b=d("n-col"),w=d("n-row"),$=d("n-form"),C=d("n-spin"),j=d("n-space");return q(),J(j,{justify:"center",align:"center",class:"tw-h-screen"},{default:u(()=>[c(C,{show:e.loading},{default:u(()=>[c($,{ref:"formRef",model:e.model},{default:u(()=>[c(a,{path:"username",label:"Account"},{default:u(()=>[c(n,{type:"email",value:e.model.username,"onUpdate:value":t[0]||(t[0]=x=>e.model.username=x),onKeydown:t[1]||(t[1]=z(R(()=>{},["prevent"]),["enter"]))},null,8,["value"])]),_:1}),c(a,{path:"password",label:"Password"},{default:u(()=>[c(n,{type:"password",value:e.model.password,"onUpdate:value":t[2]||(t[2]=x=>e.model.password=x),onKeydown:t[3]||(t[3]=z(R(()=>{},["prevent"]),["enter"]))},null,8,["value"])]),_:1}),c(w,{gutter:[0,24]},{default:u(()=>[c(b,{span:24},{default:u(()=>[Y("div",be,[c(m,{disabled:e.model.username===""||e.model.password==="",round:"",type:"primary",onClick:e.login},{default:u(()=>[Q(" Submit ")]),_:1},8,["disabled","onClick"])])]),_:1})]),_:1})]),_:1},8,["model"])]),_:1},8,["show"])]),_:1})}const Se=U(ve,[["render",we]]);export{Se as default};
