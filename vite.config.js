aae5a661f4334e50fd312d1c02c9fc9bedafe10d
Daylily <daylily@mac>
2023-05-20 17:50:46 +0800

    solve _plugin-vue_export-helper

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g
+const DRIVE_LETTER_REGEX = /^[a-z]:/i
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue({
@@ -18,6 +22,22 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName (name) {
+          const match = DRIVE_LETTER_REGEX.exec(name)
+          const driveLetter = match ? match[0] : ''
+          // substr is a die out code，so use slice
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+          )
+        }
+      }
     }
   },
   base: process.env.NODE_ENV === 'production' ? '/meowforest/' : ''
